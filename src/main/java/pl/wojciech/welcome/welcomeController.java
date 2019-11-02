34ac81044729f9cd7615af56a148e93256db247b
Wojciech <wojciech.gladalski@gmail.com>
2019-11-02 11:00:28 +0100

    initial controller

--- src/main/java/pl/wojciech/welcome/welcomeController.java
+++ src/main/java/pl/wojciech/welcome/welcomeController.java
@@ -2,6 +2,10 @@ package pl.wojciech.welcome;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class welcomeController {
@@ -9,5 +13,12 @@ public class welcomeController {
     @GetMapping("/")
     public String welcomePage() {
         return "welcome";
+    }
+
+    @PostMapping("/")
+    public String welcomePage(HttpServletRequest request, HttpSession sess){
+        String name = request.getParameter("nick");
+        sess.setAttribute("nick", name);
+        return "hi";
     }
 }

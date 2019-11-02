8271d08435006183c47e5c4077cb5d8365153f08
Wojciech <wojciech.gladalski@gmail.com>
2019-11-02 10:43:07 +0100

    added src folder

---
+++ src/main/java/pl/wojciech/app/AppInitializer.java
@@ -0,0 +1,38 @@
+package pl.wojciech.app;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import pl.wojciech.config.AppConfig;
+
+import javax.servlet.*;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+
+        ctx.register(AppConfig.class);
+
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet =
+                servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+
+        servlet.addMapping("/");
+
+        FilterRegistration.Dynamic filter =
+                servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+
+        filter.setInitParameter("encoding", "UTF-8");
+
+        filter.setInitParameter("forceEncoding", "true");
+
+        filter.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
---
+++ src/main/java/pl/wojciech/welcome/welcomeController.java
@@ -0,0 +1,13 @@
+package pl.wojciech.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class welcomeController {
+
+    @GetMapping("/")
+    public String welcomePage() {
+        return "welcome";
+    }
+}

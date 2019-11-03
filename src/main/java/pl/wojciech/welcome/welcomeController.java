d3172294ba3352c821bc3c3154793d5c12024ae0
Wojciech <wojciech.gladalski@gmail.com>
2019-11-03 23:47:53 +0100

    changed controller

--- src/main/java/pl/wojciech/welcome/welcomeController.java
+++ src/main/java/pl/wojciech/welcome/welcomeController.java
@@ -1,8 +1,8 @@
 package pl.wojciech.welcome;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,15 +10,15 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class welcomeController {
 
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcomePage() {
         return "welcome";
     }
 
-    @PostMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.POST)
     public String welcomePage(HttpServletRequest request, HttpSession sess){
         String name = request.getParameter("nick");
         sess.setAttribute("nick", name);
-        return "hi";
+        return "redirect:/chat";
     }
 }

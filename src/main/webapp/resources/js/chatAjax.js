c91a6ea82da3dbd710a23973e0aaf500d35f1e48
Wojciech <wojciech.gladalski@gmail.com>
2019-11-14 21:57:40 +0100

    added initial chat method

--- src/main/webapp/resources/js/chatAjax.js
+++ src/main/webapp/resources/js/chatAjax.js
@@ -6,25 +6,31 @@ $(function () {
 
     let serverURL
     let closeURL
+    let initURL
 
     let nrOfIP = section.data('server')
 
     if (nrOfIP == true) {
         serverURL = 'http://localhost:8080/chat'
         closeURL = 'http://localhost:8080/close'
+        initURL = 'http://localhost:8080/initChat'
     } else if (nrOfIP == 'unknown'){
         serverURL = 'unknown'
         closeURL = 'unknown'
+        initURL = 'unknown'
         header.css('display', 'block')
     } else {
         serverURL = `http://192.168.1.${nrOfIP}:8080/chat`
         closeURL = `http://192.168.1.${nrOfIP}:8080/close`
+        initURL = `http://192.168.1.${nrOfIP}:8080/initChat`
     }
 
     button.on('click', function (e) {
         e.preventDefault()
         addMessage(input, serverURL)
     })
+
+    getChat(section, initURL)
 
     setInterval(function() {
         checkChat(section, serverURL)
@@ -36,23 +42,31 @@ $(function () {
 
 })
 
-// function getChat(section) {
-//     section.empty();
-//
-//     $.ajax(
-//         {
-//             url: "http://localhost:8080/chat",
-//             type: "GET",
-//             dataType: "json"
-//         }
-//     ).done(function (response) {
-//         console.log('getting messages...')
-//
-//         response.forEach(function (item) {
-//             section.append(`<p>${item.author}: ${item.message} (${item.time})</p>`)
-//         })
-//     });
-// }
+function getChat(section, initURL) {
+
+    $.ajax(
+        {
+            url: `${initURL}`,
+            type: "GET",
+            dataType: "json"
+        }
+    ).done(function (response) {
+
+        response.forEach(function (item) {
+            section.append(`<p>${item.author}: ${item.message} (${item.time})</p>`)
+
+            if (section.children().length > 15) {
+
+                let toRemove = section.children().length - 15
+
+                for (let i = 0; i < toRemove; i++) {
+                    section.children().eq(i).remove()
+                }
+            }
+        })
+
+    });
+}
 
 function addMessage(input, serverURL) {
     console.log('trying to send message...')
